9a835d431b859acb42e5c3b128901f056756c283
ardiansyah.rangkuti <ardiansyah.rangkuti@794e06db-98e4-1587-924d-b8261c5bc7bc>
2011-11-29 04:59:16 +0000

    create activityManager and ActivityManagerImpl for view

--- core/src/main/java/com/aprisma/opensource/timesheet/dao/jpa/ActivityDaoJpa.java
+++ core/src/main/java/com/aprisma/opensource/timesheet/dao/jpa/ActivityDaoJpa.java
@@ -9,6 +9,7 @@ import com.aprisma.opensource.timesheet.dao.ActivityDao;
 import com.aprisma.opensource.timesheet.model.Activity;
 
 
+import java.sql.Date;
 import org.appfuse.dao.jpa.GenericDaoJpa;
 import org.springframework.stereotype.Repository;
 
@@ -18,17 +19,22 @@ import javax.persistence.Query;
  *
  * @author user
  */
-@Repository("ActivityDao")
+@Repository("activityDao")
 public class ActivityDaoJpa extends GenericDaoJpa<Activity, Long> implements ActivityDao {
 
     public ActivityDaoJpa() {
         super(Activity.class);
     }
-
+ 
     @Override
-    public List<Activity> findByActivityUser(String activityUser) {
-        Query q = getEntityManager().createQuery("select a from Activity a where a.activityUser=?");
-        q.setParameter(1, activityUser);
+    public List<Activity> findByActivityWeek(Long userId, Date firstDate, Date endDate) {
+        
+        
+        Query q = getEntityManager().createQuery("select a from Activity a where a.activityUser=? AND a.activityDate >= ? AND a.activityDate <= ?");
+        
+        q.setParameter(1, userId);
+        q.setParameter(2, firstDate);
+        q.setParameter(3, endDate);
         return q.getResultList();
     }
     
