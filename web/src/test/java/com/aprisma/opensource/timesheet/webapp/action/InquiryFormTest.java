1a0fad15eb2756fd18e99f07d6c836c415260fab
martinhendrawan <martinhendrawan@gmail.com>
2011-11-29 06:04:10 +0000

    Edit module timesheet-web-jsf ========================== + Update InquiryFormTest

--- web/src/test/java/com/aprisma/opensource/timesheet/webapp/action/InquiryFormTest.java
+++ web/src/test/java/com/aprisma/opensource/timesheet/webapp/action/InquiryFormTest.java
@@ -3,7 +3,9 @@ package com.aprisma.opensource.timesheet.webapp.action;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,12 +15,44 @@ import java.util.Calendar;
  */
 public class InquiryFormTest {
 
+
+
     @Test
     public void getSpecificYears() throws Exception
     {
 
        InquiryForm iform = new InquiryForm();
-       Assert.assertEquals( "ss", iform.getYears() );
+       //Assert.assertEquals( "ss", iform.getYears() );
 
     }
+
+    @Test
+    public void getAvailableWeekBasedOnMonth() throws Exception
+    {
+        InquiryForm iform = new InquiryForm();
+       iform.setMonth("11");
+        iform.setYear("2012");
+         iform.getWeeksOfMonth()  ;
+
+        /*
+         SimpleDateFormat sdf;
+         Calendar cal;
+         Date date;
+         int week;
+         String sample = "12/25/1979";
+         sdf = new SimpleDateFormat("MM/dd/yyyy");
+         date = sdf.parse(sample);
+         cal = Calendar.getInstance();
+         cal.setTime(date);
+
+         week = cal.get( Calendar.WEEK_OF_MONTH );
+         */
+        //iform.getWeeksOfMonth();
+
+
+
+    }
+
+
+
 }
