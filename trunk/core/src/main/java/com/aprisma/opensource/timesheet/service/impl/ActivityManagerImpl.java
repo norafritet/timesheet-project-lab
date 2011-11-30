cce4a562e4fd05c64efcbcfeddb1303ad3f7a794
Agus.Ramdan@gmail.com <Agus.Ramdan@gmail.com@794e06db-98e4-1587-924d-b8261c5bc7bc>
2011-11-30 11:24:39 +0000

    Commit commit activity Dao , Service

--- trunk/core/src/main/java/com/aprisma/opensource/timesheet/service/impl/ActivityManagerImpl.java
+++ trunk/core/src/main/java/com/aprisma/opensource/timesheet/service/impl/ActivityManagerImpl.java
@@ -4,51 +4,126 @@
  */
 package com.aprisma.opensource.timesheet.service.impl;
 
-
 import com.aprisma.opensource.timesheet.dao.ActivityDao;
 import com.aprisma.opensource.timesheet.model.Activity;
 import com.aprisma.opensource.timesheet.service.ActivityManager;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
 import org.appfuse.service.impl.GenericManagerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 /**
  *
  * @author user
  */
 @Service("activityManager")
-public class ActivityManagerImpl extends GenericManagerImpl<Activity,Long> implements ActivityManager{
-    
-    ActivityDao activityDao;
-    
-    public ActivityManagerImpl() {};
+public class ActivityManagerImpl extends GenericManagerImpl<Activity, Long> implements ActivityManager {
+
+    protected ActivityDao activityDao;
+
+    public ActivityManagerImpl() {
+    }
+
+    ;
     
     @Autowired
     public ActivityManagerImpl(ActivityDao activityDao) {
         super(activityDao);
         this.activityDao = activityDao;
     }
-     
+
+    public void setActivityDao(ActivityDao activityDao) {
+        super.dao = this.activityDao = activityDao;
+    }
+
     @Override
     public List<Activity> findByActivityWeek(Long userId, Date firstDate, Date endDate) {
-        try{
-        System.out.println("testing = "+userId+" "+firstDate+" "+endDate);
-        
-        List a =  activityDao.findByActivityWeek(userId, firstDate, endDate);
-        }
-        catch(Exception e){
-            System.out.println("error = "+e);
+        try {
+            System.out.println("testing = " + userId + " " + firstDate + " " + endDate);
+
+            List a = activityDao.findByActivityWeek(userId, firstDate, endDate);
+        } catch (Exception e) {
+            System.out.println("error = " + e);
         }
         List b = null;
         return b;
     }
 
-    @Override
-    public JRDataSource getJRDataSourceActivity(Long id, Date firstDate, Date endDate) {
-        return new JREmptyDataSource(1);
+    private Map<String, ?> convertActivityToMap(Activity activity) {
+
+        Map<String, Object> mapReport = new HashMap();
+        mapReport.put("date_activity", activity.getActivityDate());
+        mapReport.put("time_from", activity.getActivityDate());
+        mapReport.put("time_to", activity.getActivityDate());
+        mapReport.put("code", activity.getType());
+        mapReport.put("activities", activity.getName());
+        mapReport.put("case", activity.getActivityCase());
+        mapReport.put("i_center_no", activity.getIcenterNo());
+        mapReport.put("status", activity.getActivityStatus());
+        mapReport.put("location", activity.getLocation());
+        mapReport.put("remark", activity.getRemark());
+        return mapReport;
+
     }
 
+    @Override
+    public JRDataSource getJRDataSourceActivity(Long userId, Date firstDate, Date endDate) {
+
+        List<Activity> activityList = activityDao.findByActivityWeek(userId, firstDate, endDate);
+        List<Map<String, ?>> listOneDay, listAllDays = new ArrayList();
+        java.sql.Date currentDate;
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(firstDate);
+
+        currentDate = new java.sql.Date(calendar.getTime().getTime());
+
+        listOneDay = new ArrayList<Map<String, ?>>();
+        Map mapReport;
+        Activity activity;
+        boolean containtActivity = false;
+        Iterator<Activity> it = activityList.iterator();
+        if (it.hasNext()) {
+            activity = it.next();
+            containtActivity = true;
+            while(containtActivity){
+                if (activity.getActivityDate().after(currentDate)) {
+                    // NO ACTIVITY TODAY
+                    listAllDays.add(mapReport = new HashMap());
+                    mapReport.put("date_activity", currentDate);
+                    
+                    calendar.add(Calendar.DATE, 1);
+                    currentDate = new java.sql.Date(calendar.getTime().getTime());
+                } else {
+                    listOneDay = new ArrayList();
+                    while(containtActivity && activity.getActivityDate().toString().equals(currentDate.toString())){
+                        listOneDay.add(mapReport = convertActivityToMap( activity));
+                        containtActivity = it.hasNext();
+                        activity = containtActivity?it.next():null;
+                    }
+                    listAllDays.addAll(listOneDay);
+                }
+            }
+        }
+        calendar.add(Calendar.DATE, 1);
+        currentDate = new java.sql.Date(calendar.getTime().getTime());
+        while (endDate.after(currentDate) || endDate.toString().equals(currentDate.toString())) {
+            // NO ACTIVITY TODAY
+            listAllDays.add(mapReport = new HashMap());
+            mapReport.put("date_activity", new java.sql.Date(currentDate.getTime()));
+            
+            calendar.add(Calendar.DATE, 1);
+            currentDate = new java.sql.Date(calendar.getTime().getTime());
+        }
+        return new JRMapCollectionDataSource(listAllDays);
+    }
 }
--- trunk/core/src/test/java/com/aprisma/opensource/timesheet/service/impl/ActivityManagerImplTest.java
+++ trunk/core/src/test/java/com/aprisma/opensource/timesheet/service/impl/ActivityManagerImplTest.java
@@ -4,11 +4,22 @@
  */
 package com.aprisma.opensource.timesheet.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.sql.Time;
+import org.appfuse.model.User;
+import com.aprisma.opensource.timesheet.model.Activity;
+import java.util.List;
+import java.util.ArrayList;
+import mockit.Mocked;
 import com.aprisma.opensource.timesheet.dao.ActivityDao;
 import net.sf.jasperreports.engine.JRDataSource;
 import java.sql.Date;
+import java.util.Arrays;
 import mockit.Injectable;
+import mockit.NonStrictExpectations;
 import mockit.Tested;
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
 import org.junit.Ignore;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -22,22 +33,16 @@ import static org.junit.Assert.*;
  * @author devxpbox
  */
 public class ActivityManagerImplTest {
-    @Tested private ActivityManagerImpl activityManager;
-    @Injectable private ActivityDao activityDao;
+    private ActivityManagerImpl activityManagerImpl;
     
-    public ActivityManagerImplTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
+    @Mocked private ActivityDao activityDao;
+    
+ 
     
     @Before
     public void setUp() {
+        activityManagerImpl = new ActivityManagerImpl();
+        activityManagerImpl.setActivityDao(activityDao);
     }
     
     @After
@@ -46,9 +51,154 @@ public class ActivityManagerImplTest {
 
     @Test
     public void getJRDataSourceActivity_AnyInput_ReturnNotNull() throws Exception {
-        JRDataSource result = activityManager.getJRDataSourceActivity((long)1, new Date(1), new Date(1));
+        JRDataSource result = activityManagerImpl.getJRDataSourceActivity((long)1, new Date(1), new Date(1));
         assertNotNull(result);
     }
+    @Test
+    public void getJRDataSourceActivity_SecondWeek_ReturnEightRow() throws Exception {
+        
+        final List<Activity> list = new ArrayList();
+        Activity activity;
+        User user = new User();
+        Date date = Date.valueOf("2011-01-03");
+        Time from = Time.valueOf("08:30:00");
+        Time to   = Time.valueOf("11:30:00");
+        
+        list.add(activity = new Activity());
+        activity.setActivityUser(user);
+        activity.setActivityDate(date);
+        activity.setTimeFrom(from);
+        activity.setTimeTo(to);
+        activity.setType("ANL");
+        activity.setName("Masuk lagi ");
+        activity.setActivityCase("case1");
+        activity.setIcenterNo("JIRA-12");
+        activity.setActivityStatus("Siaga1");
+        activity.setLocation("Aprisma");
+        activity.setRemark("Remark 23");
+        
+        date = Date.valueOf("2011-01-03");
+        from = Time.valueOf("13:30:00");
+        to   = Time.valueOf("17:30:00");
+        list.add(activity = new Activity());
+        activity.setActivityUser(user);
+        activity.setActivityDate(date);
+        activity.setTimeFrom(from);
+        activity.setTimeTo(to);
+        activity.setType("ANL");
+        activity.setName("Masuk lagi");
+        activity.setActivityCase("case1");
+        activity.setIcenterNo("JIRA-12");
+        activity.setActivityStatus("Siaga1");
+        activity.setLocation("Aprisma");
+        activity.setRemark("Remark 23");
+        
+        final Date fromDate = Date.valueOf("2011-01-02");
+        final Date toDate = Date.valueOf("2011-01-08");
+        
+        new NonStrictExpectations(){{activityDao.findByActivityWeek((long)1, fromDate, toDate);result=list;}};
+        
+        JRMapCollectionDataSource result = (JRMapCollectionDataSource) activityManagerImpl.getJRDataSourceActivity((long)1, fromDate, toDate);
+        
+        assertEquals(8,result.getRecordCount());
+    }   
+    
+    @Test
+    public void getJRDataSourceActivity_SecondWeek_ReturnSevenRow() throws Exception {
+        
+        final List<Activity> list = new ArrayList();
+        Activity activity;
+        User user = new User();
+        Date date = Date.valueOf("2011-01-03");
+        Time from = Time.valueOf("08:30:00");
+        Time to   = Time.valueOf("17:30:00");
+        list.add(activity = new Activity());
+        activity.setActivityUser(user);
+        activity.setActivityDate(date);
+        activity.setTimeFrom(from);
+        activity.setTimeTo(to);
+        activity.setType("ANL");
+        activity.setName("Masuk lagi ");
+        activity.setActivityCase("case1");
+        activity.setIcenterNo("JIRA-12");
+        activity.setActivityStatus("Siaga1");
+        activity.setLocation("Aprisma");
+        activity.setRemark("Remark 23");
+        
+        
+        final Date fromDate = Date.valueOf("2011-01-02");
+        final Date toDate = Date.valueOf("2011-01-08");
+        
+        new NonStrictExpectations(){{activityDao.findByActivityWeek((long)1, fromDate, toDate);result=list;}};
+        
+        JRMapCollectionDataSource result = (JRMapCollectionDataSource) activityManagerImpl.getJRDataSourceActivity((long)1, fromDate, toDate);
+        
+        assertEquals(7,result.getRecordCount());
+    }   
+    
+//    @Test
+//    public void getJRDataSourceActivity_SecondWeek_ReturnFullJRDataSource() throws Exception {
+//        
+//        final List<Activity> list = new ArrayList();
+//        Activity activity;
+//        User user = new User();
+//        Date date = Date.valueOf("2011-01-03");
+//        Time from = Time.valueOf("08:30:00");
+//        Time to   = Time.valueOf("17:30:00");
+//        list.add(activity = new Activity());
+//        activity.setActivityUser(user);
+//        activity.setActivityDate(date);
+//        activity.setTimeFrom(from);
+//        activity.setTimeTo(to);
+//        activity.setType("ANL");
+//        activity.setName("Masuk lagi");
+//        activity.setActivityCase("case1");
+//        activity.setIcenterNo("JIRA-12");
+//        activity.setActivityStatus("Siaga1");
+//        activity.setLocation("Aprisma");
+//        activity.setRemark("Remark 23");
+//        final Date fromDate = Date.valueOf("2011-01-02");
+//        final Date toDate = Date.valueOf("2011-01-08");
+//        
+//        Map mapReport; 
+//        List<Map> listResult = new ArrayList<Map>();
+//        
+//        listResult.add(mapReport= new HashMap());
+//        mapReport.put("date_activity", Date.valueOf("2011-01-02"));
+//        
+//        listResult.add(mapReport= new HashMap());
+//        mapReport.put("date_activity", activity.getActivityDate());
+//        mapReport.put("time_from", activity.getActivityDate());    
+//        mapReport.put("time_to", activity.getActivityDate());  
+//        mapReport.put("code", activity.getType());
+//        mapReport.put("activities", activity.getName());    
+//        mapReport.put("case", activity.getActivityCase());  
+//        mapReport.put("i_center_no", activity.getIcenterNo());
+//        mapReport.put("status", activity.getActivityStatus());    
+//        mapReport.put("location", activity.getLocation());  
+//        mapReport.put("remark", activity.getRemark());
+//
+//        listResult.add(mapReport= new HashMap());
+//        mapReport.put("date_activity", Date.valueOf("2011-01-04"));
+//
+//        listResult.add(mapReport= new HashMap());
+//        mapReport.put("date_activity", Date.valueOf("2011-01-05"));
+//        
+//        listResult.add(mapReport= new HashMap());
+//        mapReport.put("date_activity", Date.valueOf("2011-01-06"));
+//        
+//        listResult.add(mapReport= new HashMap());
+//        mapReport.put("date_activity", Date.valueOf("2011-01-07"));
+//        
+//        listResult.add(mapReport= new HashMap());
+//        mapReport.put("date_activity", Date.valueOf("2011-01-08"));
+//        
+//        new NonStrictExpectations(){{activityDao.findByActivityWeek((long)1, fromDate, toDate);result=list;}};
+//        
+//        JRMapCollectionDataSource result = (JRMapCollectionDataSource) activityManagerImpl.getJRDataSourceActivity((long)1, fromDate, toDate);
+//        
+//        assertEquals(listResult,result.getData());
+//    }    
     // TODO add test methods here.
     //
     // Pola dari : The Art of Unit Tesing pathen
